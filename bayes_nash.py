5c8b05c915a4333e5edb58b326cc2db6539902d4
open-cover <open-cover@github.com>
2026-01-16 10:59:43 -0700

    expl

--- bayes_nash.py
+++ bayes_nash.py
@@ -14,11 +14,10 @@ class Player:
 
     def __init__(self, actions, omega):
         assert len(actions) == len(omega), "Mismatched actions and omega lengths"
-        # assert actions >= 1
-        # assert sum omega = 1
+        assert all(k >= 1 for k in actions), "Some types have number of actions < 1"
+        assert sum(omega) == 1, "Omega pdf does not sum to 1"
         self.n = len(actions)
         self.K = max(actions)
-        print(f"Player - n:  {self.n}, k: {self.K}")
         self.actions = np.array(actions)
         self.omega = np.array(omega)
 
@@ -31,15 +30,11 @@ class Player:
 
 class Solver:
 
-    def __init__(self, p1: Player, p2: Player, payoffs: Dict[[int, int], np.matrix]):
+    def __init__(self, p1: Player, p2: Player, payoffs: Dict[[int, int], np.array]):
         self.n1 = p1.n
         self.n2 = p2.n
         self.pairs = p1.n * p2.n
         self.omega = np.outer(p1.omega, p2.omega)[..., None]
-        print(p1.omega.shape, p2.omega.shape)
-        print("omega", self.omega.shape)
-        print("omega sum", np.sum(self.omega))
-
         self.batched_payoffs = np.zeros((self.n1, self.n2, p1.K, p2.K))
         for i in range(p1.n):
             for j in range(p2.n):
@@ -51,62 +46,103 @@ class Solver:
         self.p1 = p1
         self.p2 = p2
 
-    def get_expanded_policies(self, p1_logits, p2_logits):
-
-        x = softmax(p1_logits)
-        y = softmax(p2_logits)
-        x = np.repeat(x, self.n2, axis=0)
-        y = np.tile(y, (self.n1, 1))
-        x = x[:, None, :]
-        y = y[:, :, None]
-        return x, y
-
-    def go(self, iterations: int):
+    def go(self, iterations: int, lr: float, p: bool = False):
         p1_logits, p2_logits = self.p1.logits(), self.p2.logits()
 
         for _ in range(iterations):
+            p1_policies = softmax(p1_logits)
+            p2_policies = softmax(p2_logits)
+            p1_returns = np.einsum("ijmn,jn->ijm", self.batched_payoffs, p2_policies)
+            p2_returns = -np.einsum("im,ijmn->ijn", p1_policies, self.batched_payoffs)
 
-            x = softmax(p1_logits)
-            y = softmax(p2_logits)
-            # x = x.reshape()
-            print("logits:" , x.shape, y.shape)
-            p1_returns = np.einsum('ijmn,jn->ijm', self.batched_payoffs, y)
-            p2_returns = np.einsum('im,ijmn->ijn', x, self.batched_payoffs)
-            print("returns: ", p1_returns.shape, p2_returns.shape)
-            payoff = np.einsum('ijn,jn->ij', p2_returns, y)[..., None]
-            # payoff_same = np.einsum('im,ijm->ij', x, p1_returns)
-            print("payoff", payoff.shape)
-            # assert(payoff == payoff_same)
-            # print(payoff == payoff_same)
+            # payoff = np.einsum('ijn,jn->ij', p2_returns, p2_policies)[..., None] # mind the negative!
+            p1_payoffs = np.einsum("im,ijm->ij", p1_policies, p1_returns)[..., None]
 
-            p1_advantages = p1_returns - payoff
-            p2_advantages = p2_returns + payoff
-
-            print("advantages:", p1_advantages.shape, p2_advantages.shape)
+            p1_advantages = p1_returns - p1_payoffs
+            p2_advantages = p2_returns + p1_payoffs
 
             p1_gradient = np.sum(p1_advantages * self.omega, axis=1)
             p2_gradient = np.sum(p2_advantages * self.omega, axis=0)
-            print("gradients:", p1_gradient.shape, p2_gradient.shape)
-            assert p1_gradient.shape == (self.p1.n, self.p1.K)
-            assert p2_gradient.shape == (self.p2.n, self.p2.K)
+            # assert p1_gradient.shape == (self.p1.n, self.p1.K)
+            # assert p2_gradient.shape == (self.p2.n, self.p2.K)
 
+            p1_logits += p1_gradient
+            p2_logits += p2_gradient
+
+        return p1_logits, p2_logits
+
+    def expl(self, p1_logits: np.array, p2_logits: np.array) -> float:
+        p1_policies = softmax(p1_logits)
+        p2_policies = softmax(p2_logits)
+        p1_returns = np.einsum("ijmn,jn->ijm", self.batched_payoffs, p2_policies)
+        p2_returns = -np.einsum("im,ijmn->ijn", p1_policies, self.batched_payoffs)
+        p1_options = np.sum(self.omega * p1_returns, axis=1)
+        p2_options = np.sum(self.omega * p2_returns, axis=0)
+        p1_best = np.max(p1_options, axis=1).sum()
+        p2_best = np.max(p2_options, axis=1).sum()
+        return p1_best + p2_best
+
+
+def simple():
+
+    iterations = 10
+
+    p1 = Player([2, 3], [0.5, 0.5])
+    p2 = Player([2, 2], [0.5, 0.5])
+
+    def draw(a, b):
+        return np.zeros((a, b))
+
+    def win(a, b):
+        x = -np.ones((a, b))
+        x[0, :] = 1
+        return x
+
+    matrices = {}
+    matrices[(0, 0)] = draw(2, 2)
+    matrices[(1, 0)] = draw(3, 2)
+    matrices[(0, 1)] = win(2, 2)
+    matrices[(1, 1)] = win(3, 2)
+
+    solver = Solver(p1, p2, matrices)
+    p1_logits, p2_logits = solver.go(iterations=iterations, lr=0.01)
+    e = solver.expl(p1_logits, p2_logits)
+    print(f"expl: {e}")
 
 
 def test():
     import random
-    tries = 1000
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--iterations",
+        type=int,
+        required=True,
+        help="Number of iterations to use when solving",
+    )
+    parser.add_argument(
+        "--tries",
+        type=int,
+        required=True,
+        help="Number of Bayesian games to try solving",
+    )
+
+    args = parser.parse_args()
+
+    tries = args.tries
+    iterations = args.iterations
 
     n1 = random.randint(1, 5)
     n2 = random.randint(1, 5)
-
     k1 = [random.randint(1, 4) for _ in range(n1)]
     k2 = [random.randint(1, 4) for _ in range(n2)]
     o1 = [1.0 / n1 for _ in range(n1)]
     o2 = [1.0 / n2 for _ in range(n2)]
 
+    total_expl = 0
 
     for _ in range(tries):
-        print(f"try: {_}")
         p1 = Player(k1, o1)
         p2 = Player(k2, o2)
 
@@ -116,7 +152,13 @@ def test():
                 matrices[(i, j)] = np.random.rand(k1[i], k2[j])
 
         solver = Solver(p1, p2, matrices)
-        solver.go(1)
+        p1_logits, p2_logits = solver.go(iterations=iterations, lr=0.01)
+
+        e = solver.expl(p1_logits, p2_logits)
+        total_expl += e
+
+    print(f"Average exploitability: {total_expl / tries}")
+
 
 if __name__ == "__main__":
     test()
